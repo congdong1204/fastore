d916d1e4628ec229ccc1b764b81bde8596642981
Dong Pham <congdong1204@gmail.com>
2025-03-03 22:08:58 +0700

    serverless environment config

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,10 +1,8 @@
 'use server'
 
-import { PrismaClient } from '@prisma/client'
 import { convertToPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants'
-
-const prisma = new PrismaClient()
+import { prisma } from '@/db/prisma'
 
 // Get latest products
 export async function getLatestProducts() {
