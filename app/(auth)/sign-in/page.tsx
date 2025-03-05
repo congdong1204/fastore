07d53fa9f02af79412823120641285059eeccd16
Dong Pham <congdong1204@gmail.com>
2025-03-05 12:15:12 +0700

    auth layout and sign in page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from '@/components/ui/card'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import { APP_NAME } from '@/lib/constants'
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+}
+
+const SignInPage = () => {
+  return (
+    <div className="max-w-md mx-ato w-full">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} Logo`}
+              width={100}
+              height={100}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">{/* Form Here */}</CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default SignInPage
