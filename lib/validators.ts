b9ccc78490042b84c1d0233823b1c80989fc3621
Dong Pham <congdong1204@gmail.com>
2025-03-05 11:11:33 +0700

    sign in and sign out action

--- lib/validators.ts
+++ lib/validators.ts
@@ -27,3 +27,11 @@ export const insertProductSchema = z.object({
   banner: z.string().nullable(),
   price: currency,
 })
+
+// Schema for signing users in
+export const signInFormSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters' }),
+})
